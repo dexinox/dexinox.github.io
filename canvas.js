6562b78881832820b6035aa7dbf7f8bef4ab9bea
dexinox <49427973+dexinox@users.noreply.github.com>
2020-07-26 14:17:31 +0530

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -39,6 +39,22 @@ var mouse = {
     y: undefined
 }
 
+var maxRadius = 40;
+//var minRadius = 2;
+
+
+var colorArray = [
+    '#F15025',
+    '#FFFFFF',
+    '#E6E8E6',
+    '#CED0CE',
+    '#121212'
+];
+
+
+
+
+
 window.addEventListener('mousemove', 
     function(event) {
         mouse.x = event.x;
@@ -53,12 +69,14 @@ function Circle(x, y, dx, dy, radius) {
     this.dx = dx;
     this.dy = dy;
     this.radius = radius;
+    this.minRadius = radius;
+    this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
 
     this.draw = function() {
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         c.strokeStyle = "blue";
-        c.stroke();
+        c.fillStyle = this.color;
         c.fill();
     }
 
@@ -78,10 +96,10 @@ function Circle(x, y, dx, dy, radius) {
 
         if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50){
             
-            if (this.radius < 40){
+            if (this.radius < maxRadius){
             this.radius += 1;
             }
-        } else if (this.radius > 2) {
+        } else if (this.radius > this.minRadius) {
             this.radius -= 1;
         }
 
@@ -93,8 +111,8 @@ function Circle(x, y, dx, dy, radius) {
 
 var circleArray = [];
 
-for (var i=0; i<2000; i++) {
-    var radius = 50;
+for (var i=0; i<200; i++) {
+    var radius = Math.random() * 3 + 1;
     var x = Math.random() * (innerWidth - radius * 2) + radius;
     var y = Math.random() * (innerHeight - radius * 2) + radius;
     var dx = (Math.random() - 0.5);
